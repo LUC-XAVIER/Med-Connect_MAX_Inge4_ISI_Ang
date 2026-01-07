1f7897fe0c4e2a724714fbeea8e451550eb7ed4d
Luc-Xavier <foningxavier@gmail.com>
2026-01-07 05:05:28 +0100

    modified package.json for deploy

--- MedConnect-Back/src/config/mongodb.ts
+++ MedConnect-Back/src/config/mongodb.ts
@@ -1,24 +1,6 @@
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// import logger from '@utils/logger';
-
-// dotenv.config();
-
-// const connectMongoDB = async (): Promise<void> => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI as string);
-//     logger.info('✅ MongoDB connected successfully');
-//   } catch (error) {
-//     logger.error('❌ MongoDB connection error:', error);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectMongoDB;
-
 import { MongoClient, Db } from 'mongodb';
 import dotenv from 'dotenv';
-import logger from '@utils/logger';
+import logger from '../utils/logger';
 
 dotenv.config();
 
